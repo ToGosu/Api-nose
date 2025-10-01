6cd56f8cfa1ba7c0b9d367c618375856557e8b0a
ToGosu <santitorres032704@gmail.com>
2025-10-01 16:32:55 -0500

    Clase 27 de septiembre Crear el UUIDHelper y el ObjectHelper

---
+++ src/main/java/co/edu/uco/nose/buisness/domain/CountryDomain.java
@@ -0,0 +1,41 @@
+package co.edu.uco.nose.buisness.domain;
+
+import java.util.UUID;
+
+import co.edu.uco.nose.crosscuting.helper.TextHelper;
+import co.edu.uco.nose.crosscuting.helper.UUIDHelper;
+
+//no heredable
+public final class CountryDomain  extends Domain{
+	
+	private String name;
+	
+	public CountryDomain() {
+		super(UUIDHelper.getUUIDHelper().getDefault());
+		setName(TextHelper.getDefault());
+	}
+
+	public CountryDomain(final UUID id) {
+		super(id);
+		setName(TextHelper.getDefault());
+	}
+	
+	public CountryDomain(final UUID id,final String name) {
+		super(id);
+		this.name = name;
+	}
+
+	
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = TextHelper.getDefaultWithTrim(name);
+	}
+	
+	
+
+}
